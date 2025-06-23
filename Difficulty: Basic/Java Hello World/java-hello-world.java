8c567da8e9aef704399b36a8d3d2b5fa8258c9ae
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2025-06-23 23:52:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Hello World/java-hello-world.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void printHelloWorld() {
+        // Print "Hello World" to the console
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        // Call the function to print Hello World
+        printHelloWorld();
+    }
+}
