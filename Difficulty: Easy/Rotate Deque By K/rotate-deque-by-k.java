2d0c529994128692dd4022096b969b84afeb153b
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2025-09-26 19:05:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate Deque By K/rotate-deque-by-k.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void rotateDeque(Deque<Integer> dq, int type, int k) {
+        if(type == 1) {
+            while(k-- > 0) {
+                dq.addFirst(dq.removeLast());
+            }
+        } else {
+            while(k-- > 0) {
+                dq.addLast(dq.removeFirst());
+            }
+        }
+    }
+}
