820f786ebfd33a83db1b68d3383a6cdc4e97a6d7
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2025-09-30 18:19:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+       public ArrayList<String> binstr(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        int total = 1 << n; // 2^n
+
+        for (int i = 0; i < total; i++) {
+            String binary = String.format("%" + n + "s", Integer.toBinaryString(i))
+                                .replace(' ', '0');
+            result.add(binary);
+        }
+        return result;
+    }
+}
