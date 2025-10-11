dc27937b7f473a070ff269192607c9757cdb0509
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2025-10-11 20:35:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum path sum/maximum-path-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    long max, min = Integer.MIN_VALUE;
+    int findMaxSum(Node root) {
+        max = (long)Integer.MIN_VALUE;
+        findMaxSum2(root);
+        return (int)max;
+    }
+    long findMaxSum2(Node root){
+        if(root == null){
+            return min;
+        }
+        long left = findMaxSum2(root.left);
+        long right = findMaxSum2(root.right);
+        
+        long max2 = Math.max((long)root.data,Math.max((long)root.data+left,(long)root.data+right));
+        max = Math.max(max,Math.max(max2,Math.max(left,Math.max(right,left+right+(long)root.data))));
+    
+        return max2;
+    }
+}
