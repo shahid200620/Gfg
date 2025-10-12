6e091b5caeee71d86f071f604d5baa5c5ec5791e
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2025-10-12 17:00:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distribute Candies/distribute-candies.java
@@ -0,0 +1,15 @@
+class Solution {
+    int ans = 0;
+    public int distCandy(Node root) {
+        helper(root);
+        return ans;
+    }
+    int helper (Node root){
+        if (root == null)  return 0;
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+        ans += Math.abs(left)+Math.abs(right);
+        return root.data+left+right-1;
+    }
+}
