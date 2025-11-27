7114ae196d55a5b109918be11c26dd8f1ab98042
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2025-11-27 21:21:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Subsets Xor Sum/all-subsets-xor-sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    int subsetXORSum(int arr[]) {
+        int or = 0;
+        for (int x : arr) {
+            or |= x;    
+        }
+        int n = arr.length;
+        return or * (1 << (n - 1));
+    }
+}
