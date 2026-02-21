22d5f376e572a81a6e2859caa72d0de9ec3ae649
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-02-21 13:06:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,13 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
-        // code here
-        Arrays.sort(citations);
+       Arrays.sort(citations);
         int n = citations.length;
-        int idx = 0;
-        
-        while(idx < n && citations[n-1-idx] > idx){
-            idx++;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
+            }
         }
-        return idx;
+        return 0;
     }
 }
+
